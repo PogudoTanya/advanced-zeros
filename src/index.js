a83d1b321b5fd577c5f6ff8e5c57ee56ce6c5851
PogudoTanya <34543226+PogudoTanya@users.noreply.github.com>
2019-03-24 04:00:14 +0300

    true solution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementatio
-  var ops=1000000000;
-  function ans(ops);
+module.exports =function getZerosCount(number, base) {
+   var ans=1e7;
 for(var i=2;i<=base; i++)
 {
   if(base%i==0)
@@ -15,10 +13,11 @@ while(base%i==0)
 var cnt_base2=0;
 var t=number;
 while(t/i>0){
-  cnt_base2+=t/i;
+  (cnt_base2+=Math.floor(t/i));
   t/=i;
 }
-ans=min(ans,cnt_base2/cnt_base1);
+ops=Math.floor(cnt_base2/cnt_base1);
+ ans= Math.min(ans,ops);
   }
 }
 return ans;
