0d7bc95d536b82631cc067ada2c3bb90285c847a
PogudoTanya <34543226+PogudoTanya@users.noreply.github.com>
2019-03-24 03:59:16 +0300

    add txt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+   var ans=1e7;
+for(var i=2;i<=base; i++)
+{
+  if(base%i==0)
+  {
+var cnt_base1=0;
+while(base%i==0)
+{
+  cnt_base1++;
+  base/=i;
+}
+var cnt_base2=0;
+var t=number;
+while(t/i>0){
+  (cnt_base2+=Math.floor(t/i));
+  t/=i;
+}
+ops=Math.floor(cnt_base2/cnt_base1);
+ ans= Math.min(ans,ops);
+  }
+}
+return ans;
+
 }
